7833ad4422145ad01757b8adcdc902434ef694e3
Ito Aghayere <itohan.aghayere@gmail.com>
2024-07-11 02:48:24 -0700

    Exercise 4 here

--- app.js
+++ app.js
@@ -34,8 +34,22 @@ Exercise 3: Insert at the beginning
 Complete Exercise 3 in the space below:
 */
 
-const foods = ["pizza", "cheeseburger"];  
-const firstElement = foods.shift()
-console.log('Exercise 3 result:', foods);
-console.log(firstElement)
+// const foods = ["pizza", "cheeseburger"];  
+// const firstElement = foods.shift()
+// console.log('Exercise 3 result:', foods);
+// console.log(firstElement)
 
+/*
+Exercise 4: Access an array element
+
+1) Retrieve the 'pizza' string from the array based on its position (index) in
+   the array.  
+
+2) Assign it to a variable called `favFood`.
+
+Complete Exercise 4 in the space below:
+*/
+
+const foods = ["pizza", "cheeseburger"];  
+const favFood = foods[0]
+console.log('Exercise 4 result:', favFood);
